21f916887505790267e130d17173f569399931a7
Andreas Gunnarsson <tailsfang@gmail.com>
2022-06-04 00:36:44 +0200

    Add project files.

---
+++ Models/World.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NbodyWithUI.Models
+{
+    internal class World
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public List<Particle> Particles { get; set; }
+    }
+}
