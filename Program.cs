edfafadfbeb9278881283f08ca320e8ef2d6728f
Andreas Gunnarsson <tailsfang@gmail.com>
2022-06-04 02:41:12 +0200

    ...

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using NbodyWithUI.Models;
 
-Particle p1 = new Particle(10, (0, 1));
-Particle p2 = new Particle(10, (1, 1));
+Particle p1 = new Particle(10,1, (0, 0));
+Particle p2 = new Particle(10,1, (2, 0));
 
 p1.totalDistplacementDuringStep(p2, 2);
